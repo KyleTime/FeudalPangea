adf1ca732b3b73981f25c39e35c391d325570015
Kyle Burnett <kylethompsonburnett@gmail.com>
2025-06-09 16:35:26 -0700

    UserInterface: Added a pause menu and some better time pausing functionality with TimeManager. Also some crude art assets.

---
+++ FeudalPangaeaCode/Menu/PauseMenu.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class PauseMenu : Control
+{
+    [Export] public Button cont;
+    [Export] public Button quit;
+
+    public override void _Ready()
+    {
+        cont.ButtonDown += ContinueButton;
+        quit.ButtonDown += QuitButton;
+    }
+
+    public void QuitButton()
+    {
+        GetTree().Quit();
+    }
+
+    public void ContinueButton()
+    {
+        UnPause();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (Input.IsActionJustPressed("QUIT"))
+        {
+            if (Visible)
+            {
+                UnPause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Visible;
+        Visible = true;
+        TimeManager.PauseTime(true);
+    }
+
+    public async void UnPause()
+    {
+        await Task.Delay(10);
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        Visible = false;
+        TimeManager.PauseTime(false);
+    }
+}
